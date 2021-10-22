fc2505f3c77251cc5ba0afbd424e30537d9d28cd
aalhad.k <aalhad.k@media.net>
2021-10-22 21:06:08 +0530

    removed control panel link from index page

--- index.php
+++ index.php
@@ -12,6 +12,4 @@ include_once "authenticate.php";
 
 <a href="updateLB.php">Update LB</a>
 <br/>
-<a href="controlPanel.php">League control panel</a>
-<br/>
 <a href="updatePlayers.php">Update players</a>
