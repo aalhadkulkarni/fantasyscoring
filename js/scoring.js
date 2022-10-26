e2bec28da802064c3447eb8f8e1a54a17cbb4689
aalhadkulkarni <aalhad.kulkarni@gmail.com>
2022-10-27 04:39:20 +0530

    test

--- js/scoring.js
+++ js/scoring.js
@@ -773,12 +773,17 @@ function getIntlMatchData(matchId) {
     // }
 
     //https://ipl-stats-sports-mechanic.s3.ap-south-1.amazonaws.com/ipl/feeds/456-Innings1.js?onScoring=_jqjsp&_1648321460797=
-    $.getJSON("https://api.icc.cdp.pulselive.com/fixtures/" + matchId + "/scoring").done(function (data) {
+    /*$.getJSON("https://api.icc.cdp.pulselive.com/fixtures/" + matchId + "/scoring").done(function (data) {
         console.log(data);
         onScoring(data);
-     })
+     })*/
     //actualMatchNo = parseInt(matchNo);
     //fetchInnings(1);
+
+    $.ajax({
+        url: "https://api.icc.cdp.pulselive.com/fixtures/" + matchId + "/scoring",
+        dataType: 'jsonp',
+    });
 }
 
 getFirebaseData();
