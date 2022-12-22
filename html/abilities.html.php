a045e86922717f8a1da253cc852b020105ff3b9d
aalhadkulkarni <aalhad.kulkarni@gmail.com>
2022-12-22 14:17:41 +0530

    abilities

--- html/abilities.html.php
+++ html/abilities.html.php
@@ -2,4 +2,24 @@
 ?>
 
 <html>
+    <head>
+        <script>
+            function getResult() {
+                console.log('Ok');
+            }
+        </script>
+    </head>
+    <body>
+        <textarea id="types" rows="20" columns="20">
+
+        </textarea>
+
+        <textarea id="targets" rows="20" columns="20">
+
+        </textarea>
+
+        <br/>
+
+        <button onclick="getResult()">Process</buttonm>
+    </body>
 </html>
